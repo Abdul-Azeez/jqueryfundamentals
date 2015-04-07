674f8e32fc46b52093235d12dc13bde553504023
Abdul-Azeez <isiaq@dealdey.com>
2015-04-07 11:48:57 +0100

    Update tabs.js

--- jqfundamentals-master/exercises/chapter4/js/tabs.js
+++ jqfundamentals-master/exercises/chapter4/js/tabs.js
@@ -2,25 +2,29 @@ function TabbedNavigation() {
 
 };
 
-TabbedNavigation.prototype.addTabNav= function () {
-  var $modules = $('div.module')
-  console.log($modules);
-  var $firstmodule =$("div.module").first();
-  var $unordered = $firstmodule.before("<ul></ul>");
-  $modules.each(function () {
+TabbedNavigation.prototype.addTabNav= function () { 
+  var $modules = $('div.module').hide();
+  var $nav = $('<ul/>')
+    .addClass('tabs')
+    .insertBefore($modules.eq(0));  
+  $modules.each(function() {
     var $module = $(this);
-    var $textValue = $module.find('h2:first').text();
-    var $tabvalue = $('<li>' + $textValue +'<li>').appendTo($unordered);
-    $tabvalue.bind('click', function() {
-      $tabvalue.addClass('current').siblings('').removeClass('current');
-      $module.show().siblings('.module').hide()
+    var $title = $module.find('h2');
+
+    var $tab = $('<li>' + $title.text() + '</li>')
+      .appendTo($nav);
+
+    $tab.click(function() {
+      $tab.addClass('current').siblings().removeClass('current');
+      $module.show().siblings('.module').hide();
     });
-    $unordered.find('li:first').addClass('current');
-    $modules.eq(0).show();
   });
- };
+    $modules.eq(0).show();
+  $nav.find('li:first').addClass('current');
+};
 
 $(document).ready(function() {
   var tabnav = new TabbedNavigation();
   tabnav.addTabNav();
 });
+
