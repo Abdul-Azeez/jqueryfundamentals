9ec6265c57407a9fe4bdeb7ee1465afc0fdda1ec
Abdul-Azeez <isiaq@dealdey.com>
2015-04-08 10:16:59 +0100

    Update load.js

--- jqfundamentals-master/exercises/chapter6/js/load.js
+++ jqfundamentals-master/exercises/chapter6/js/load.js
@@ -2,35 +2,38 @@ function LoadWebpage() {
 };
 
 LoadWebpage.prototype.loadBlog = function() {
-  var $headers = $('#blog h3');
-  _that = this;
-  $headers.each(function() {
-    var $this = $(this);
-  })
-  .click(function(e) {
-    e.preventDefault();
-
+  var $blog = $('#blog');
+  var headers = $blog.find('h3')
+  headers.each(function() {
+  var $this = $(this);
+    var $target = $('<div/>').insertAfter($this);
+    $this.data('target', $target);
+    })
+  headers.click(function(e) {
+   e.preventDefault();
     var $this = $(this);
     var link = $this.find('a').attr('href');
     var tempArray = link.split('#');
     var divId = '#' + tempArray[1];
-    var $newDiv = $('<div id=\'a\'></div>').insertAfter($this);
-    $this.data('newDiv', $newDiv);
-    $newDiv.load("data/blog.html " + divId, function(response, status, xhr) {
+      console.log('hi');
+    var $target = $this.data('target');
+    $target.load("data/blog.html " + divId, function(response, status, xhr) {
     if (status == "success") {
-      $currentelement= $newDiv
-      $currentelement.slideToggle( "slow" );
+      $currentelement= $target;
+      $this.not($target).hide();
+      
+      console.log('hello');
     } 
     if (status == "error") {
-        var msg = "Sorry but there was an error: ";
-        alert(msg + xhr.status + " " + xhr.statusText);
+      var msg = "Sorry but there was an error: ";
+      alert(msg + xhr.status + " " + xhr.statusText);
       }
     });
         
   });
 };
 
-  $(document).ready(function() {
+$(document).ready(function() {
     var loadweb = new LoadWebpage();
     loadweb.loadBlog();
-  });
+});
