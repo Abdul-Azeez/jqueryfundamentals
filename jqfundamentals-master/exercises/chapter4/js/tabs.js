2e8bcc38a5559c2837f6cc53a43b4b48a613bce3
Abdul-Azeez <isiaq@dealdey.com>
2015-04-14 09:17:09 +0100

    Done

--- jqfundamentals-master/exercises/chapter4/js/tabs.js
+++ jqfundamentals-master/exercises/chapter4/js/tabs.js
@@ -3,27 +3,28 @@ function TabbedNavigation () {
 
 TabbedNavigation.prototype.hideModules = function() {
   var $modules = $('div.module').hide();
-  that = this
+  var that = this;
   var $nav = $('<ul/>')
     .addClass('tabs')
     .insertBefore($modules.eq(0));
   $modules.each(function() { 
     var $module = $(this)
-    that.putContent($nav, $module)
+    that.createTabs($nav, $module, $modules)
   })
-  $modules.eq(0).show();
-  $nav.find('li:first').addClass('current');
 }
-TabbedNavigation.prototype.putContent = function($nav, $module) {
-    var $title = $module.find('h2');
-    var $tab = $('<li>' + $title.text() + '</li>')
-      .appendTo($nav);
-    $tab.click(function() {
-      $module.show().siblings('.module').hide();
-    });
+TabbedNavigation.prototype.createTabs = function($nav, $module, $modules) {
+  var $title = $module.find('h2');
+  var $tab = $('<li>' + $title.text() + '</li>')
+    .appendTo($nav);
+  $tab.click(function() {
+    $module.show().siblings('.module').hide();
+    $module.addClass('current');
+  });
+  $modules.eq(0).show();
 };
 
 $(document).ready(function() {
   var tabnav = new TabbedNavigation();
   tabnav.hideModules();
+});
 });
